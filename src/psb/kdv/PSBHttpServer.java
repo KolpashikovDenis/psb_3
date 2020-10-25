29cdc97cb418900043ba48576b82c419c9a9a91f
KolpaschikovDenis <kolpashikov.denis@gmail.com>
2020-10-25 19:15:56 +0400

    А... что-то делаю...

--- src/psb/kdv/PSBHttpServer.java
+++ src/psb/kdv/PSBHttpServer.java
@@ -1,8 +1,24 @@
 package psb.kdv;
 
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.File;
+import java.net.URISyntaxException;
+
 public class PSBHttpServer {
 
     public static void main(String[] args) {
+        String fullName, jarName;
 	// write your code here
+        if (args.length != 1){
+            System.out.println("Usage:\n\tjava -jar ");
+        }
+        try {
+            fullName = new File(PSBHttpServer.class.getProtectionDomain().getCodeSource().getLocation().toURI()).toString();
+            jarName = fullName.substring(fullName.lastIndexOf(System.getProperty("line.separator"))+1);
+            System.out.println(jarName);
+        }catch(URISyntaxException e){
+            e.printStackTrace();
+        }
     }
 }
