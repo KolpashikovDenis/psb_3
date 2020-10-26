77de2d5524f69ba5e1b759966bfe9138da441428
KolpaschikovDenis <kolpashikov.denis@gmail.com>
2020-10-26 20:07:39 +0400

    А... что-то делаю...# 2

---
+++ src/psb/kdv/PSBHttpHandler.java
@@ -0,0 +1,13 @@
+package psb.kdv;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+
+public class PSBHttpHandler implements HttpHandler {
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException {
+
+    }
+}
--- src/psb/kdv/PSBHttpServer.java
+++ src/psb/kdv/PSBHttpServer.java
@@ -1,24 +1,44 @@
 package psb.kdv;
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.net.URISyntaxException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class PSBHttpServer {
+    static HttpServer myserver;
 
     public static void main(String[] args) {
         String fullName, jarName;
 	// write your code here
         if (args.length != 1){
-            System.out.println("Usage:\n\tjava -jar ");
+            System.out.println("Usage:\n\tjava -jar PSBHttpServer.jar <port>");
         }
-        try {
-            fullName = new File(PSBHttpServer.class.getProtectionDomain().getCodeSource().getLocation().toURI()).toString();
-            jarName = fullName.substring(fullName.lastIndexOf(System.getProperty("line.separator"))+1);
-            System.out.println(jarName);
-        }catch(URISyntaxException e){
+
+        int port = Integer.parseInt(args[0]);
+
+        ThreadPoolExecutor pePool = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
+
+        try{
+            myserver = HttpServer.create(new InetSocketAddress("localhost", port), 0);
+            myserver.createContext("/", new HttpHandler() {
+                @Override
+                public void handle(HttpExchange httpExchange) throws IOException {
+
+                }
+            });
+            myserver.setExecutor(pePool);
+            myserver.start();
+
+        }catch(Exception e){
             e.printStackTrace();
         }
+
     }
 }
