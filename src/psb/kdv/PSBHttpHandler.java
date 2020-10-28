5f226a214837e906d1e08d4f4290aaeb1328b223
KolpaschikovDenis <kolpashikov.denis@gmail.com>
2020-10-28 20:57:07 +0400

    Остались простые числа

--- src/psb/kdv/PSBHttpHandler.java
+++ src/psb/kdv/PSBHttpHandler.java
@@ -2,11 +2,17 @@ package psb.kdv;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
-import jdk.nashorn.internal.parser.JSONParser;
-
+import org.json.*;
 import java.io.*;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class PSBHttpHandler implements HttpHandler {
+    String[] opers = {"sum", "max", "min", "average", "median", "prime"};
+    String resInt = "{\r\n\t\"result\":[%d]\r\n}";
+    String resFloat = "{\r\n\t\"result\":[%f]\r\n}";
+    String resError = "{\r\n\t\"result\":[%s]\r\n}";
+
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
         String reqParamValue = null;
@@ -36,7 +42,52 @@ public class PSBHttpHandler implements HttpHandler {
             stringBuilder.append(line);
         }
 
-        JSONParser parser = new JSONParser();
+        JSONObject root = new JSONObject(stringBuilder.toString());
+        JSONArray arr = root.getJSONArray("numbers");
+        Integer[] nums = new Integer[arr.length()];
+        for(int i = 0; i < arr.length(); i++){
+            nums[i] = Integer.parseInt(arr.get(i).toString());
+        }
+
+        // операция - sum, max, min, average, median, prime
+        String oper = root.get("operation").toString();
+        stringBuilder.setLength(0);
+
+        if (oper.contains(opers[0])){
+            int sum = 0;
+            for(int n: nums){
+                sum+=n;
+            }
+            stringBuilder.append(String.format(resInt, sum));
+        } else if (oper.contains(opers[1])){
+            Arrays.sort(nums, Collections.reverseOrder());
+            stringBuilder.append(String.format(resInt, nums[0]));
+        } else if (oper.contains(opers[2])){
+            Arrays.sort(nums);
+            stringBuilder.append(String.format(resInt, nums[0]));
+        } else if (oper.contains(opers[3])){
+            double f = 0.0;
+            int sum = 0;
+            for(int n: nums){
+                sum += n;
+            }
+            f = (double)sum/nums.length;
+            stringBuilder.append(String.format(resFloat, f));
+        } else if (oper.contains(opers[4])){
+            Arrays.sort(nums);
+            // определим чётность количества значений массива
+            if((nums.length % 2) == 1){
+
+                stringBuilder.append(String.format(resInt, nums[(nums.length+1)/2-1]));
+            } else {
+                int i = nums.length/2;
+                double avg = (double)(nums[i-1]+nums[i])/2;
+                stringBuilder.append(String.format(resFloat, avg));
+            }
+        } else if (oper.contains(opers[5])){
+
+        } else {
+        }
 
 
         return stringBuilder.toString();
